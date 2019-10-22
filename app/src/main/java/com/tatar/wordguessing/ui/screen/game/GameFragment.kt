ce65fb754939aeead3842cbabd803b998515eae4
tartarJR <mustafaogun.ozturk@gmail.com>
2019-10-22 13:17:23 +0300

    Add game implementation

--- app/src/main/java/com/tatar/wordguessing/ui/screen/game/GameFragment.kt
+++ app/src/main/java/com/tatar/wordguessing/ui/screen/game/GameFragment.kt
@@ -2,39 +2,104 @@ package com.tatar.wordguessing.ui.screen.game
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-
 import com.tatar.wordguessing.R
+import com.tatar.wordguessing.data.ScoreManager
+import com.tatar.wordguessing.data.WordProvider
 import com.tatar.wordguessing.databinding.FragmentGameBinding
-import com.tatar.wordguessing.databinding.FragmentPreGameBinding
 
 
 class GameFragment : Fragment() {
+
+    private lateinit var wordProvider: WordProvider
+    private lateinit var scoreManager: ScoreManager
+
+    private lateinit var binding: FragmentGameBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentGameBinding = DataBindingUtil.inflate(
+        initWords()
+        initScore()
+        setBindings(inflater, container)
+        initViews()
+
+        return binding.root
+    }
+
+    private fun initWords() {
+        wordProvider = WordProvider()
+        wordProvider.refreshList()
+    }
+
+    private fun initScore() {
+        scoreManager = ScoreManager()
+        scoreManager.provideScore()
+    }
+
+    private fun setBindings(inflater: LayoutInflater, container: ViewGroup?) {
+        binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_game,
             container,
             false
         )
 
-        binding.correctBtn.setOnClickListener {
-            
+        binding.score = scoreManager.getScore()
+        binding.word = wordProvider.getWord()
+    }
+
+    private fun initViews() {
+        binding.correctBtn.setOnClickListener { onCorrectAnswer() }
+        binding.skipBtn.setOnClickListener { onSkipWord() }
+    }
+
+    private fun onCorrectAnswer() {
+        if (wordProvider.isWordListEmpty()) {
+            navigateToScoreFragment()
+            return
         }
 
-        binding.skipBtn.setOnClickListener {
+        displayNextWord()
+        displayIncreasedScore()
+    }
 
+    private fun displayIncreasedScore() {
+        scoreManager.increaseScore()
+        binding.score = scoreManager.getScore()
+    }
+
+    private fun onSkipWord() {
+        if (wordProvider.isWordListEmpty()) {
+            navigateToScoreFragment()
+            return
         }
 
-        return binding.root
+        displayNextWord()
+        displayDecreasedScore()
+    }
+
+    private fun displayDecreasedScore() {
+        scoreManager.decreaseScore()
+        binding.score = scoreManager.getScore()
+    }
+
+    private fun displayNextWord() {
+        binding.word = wordProvider.getWord()
+    }
+
+    private fun navigateToScoreFragment() {
+        findNavController().navigate(
+            GameFragmentDirections.actionGameFragmentToScoreFragment(
+                binding.score!!.finalScore,
+                binding.score!!.completionTimeInSeconds
+            )
+        )
     }
 }

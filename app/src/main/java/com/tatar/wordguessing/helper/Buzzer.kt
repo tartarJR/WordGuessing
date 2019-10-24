173082b20bd93123e975e1ee88b3c12ecafdfec8
tartarJR <mustafaogun.ozturk@gmail.com>
2019-10-24 14:19:35 +0300

    Add Buzzer and vibration permission

---
+++ app/src/main/java/com/tatar/wordguessing/helper/Buzzer.kt
@@ -0,0 +1,46 @@
+package com.tatar.wordguessing.helper
+
+import android.content.Context
+import android.content.Context.VIBRATOR_SERVICE
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
+
+
+class Buzzer(context: Context) {
+
+    private var buzzer: Vibrator? = context.getSystemService(VIBRATOR_SERVICE) as Vibrator?
+
+    fun buzz(buzzType: BuzzType) {
+        Log.d("Buzzer", buzzType.name)
+
+        buzzer?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                it.vibrate(
+                    VibrationEffect.createWaveform(
+                        buzzType.pattern,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
+            } else {
+                //deprecated in API 26
+                it.vibrate(buzzType.pattern, -1)
+            }
+        }
+    }
+
+    companion object {
+        private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+        private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+        private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+        private val NO_BUZZ_PATTERN = longArrayOf(0)
+    }
+
+    enum class BuzzType(val pattern: LongArray) {
+        CORRECT(CORRECT_BUZZ_PATTERN),
+        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+        COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
+        NO_BUZZ(NO_BUZZ_PATTERN)
+    }
+}
